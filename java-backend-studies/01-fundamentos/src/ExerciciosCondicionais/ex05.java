25e6361292d07de78b9ae8276a43a0ff5e10f214
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-07-25 22:04:06 -0300

    exercicio que verifica qual é o maior numero entre 3 numeros digitados

---
+++ java-backend-studies/01-fundamentos/src/ExerciciosCondicionais/ex05.java
@@ -0,0 +1,29 @@
+package ExerciciosCondicionais;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//Faça um programa que leia três números e mostre o maior deles:
+public class ex05 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double numero1, numero2, numero3;
+        System.out.println("Digite o primeiro numero: ");
+        numero1 = sc.nextDouble();
+        System.out.println("Digite o segundo numero: ");
+        numero2 = sc.nextDouble();
+        System.out.println("Digite o terceiro numero: ");
+        numero3 = sc.nextDouble();
+        if (numero1 > numero2 && numero1 >numero3) {
+            System.out.println("O maior numero foi: " + numero1);
+        }
+        else if (numero2 > numero1 && numero2 > numero3) {
+            System.out.println("O menor numero foi: " + numero2);
+        }
+        else{
+            System.out.println("O menor numero foi: " + numero3);
+        }
+    }
+}
