fd8efc5871e1bced3bbcfd75fdbd07876ce7ded1
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-07-25 21:46:01 -0300

    exercicio que recebe 2 numeros e verifica qual é o maior

--- java-backend-studies/01-fundamentos/src/ExerciciosCondicionais/ex01.java
+++ java-backend-studies/01-fundamentos/src/ExerciciosCondicionais/ex01.java
@@ -1,4 +1,25 @@
 package ExerciciosCondicionais;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+//Faça um programa que peça dois números e imprima o maior deles
 public class ex01 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double numero1, numero2;
+        System.out.println("Digite o primeiro numero: ");
+        numero1 = sc.nextDouble();
+        System.out.println("Digite o segundo numero: ");
+        numero2 = sc.nextDouble();
+
+        if (numero1 > numero2) {
+            System.out.printf("O maior numero é %.1f", numero1);
+        }
+        else {
+            System.out.printf("O maior numero é %.1f", numero2);
+        }
+    }
 }
