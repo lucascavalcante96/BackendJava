c1807405a7fac1e2299fb0a0f0863a385623dd7a
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-02 12:15:21 -0300

    reiniciando estudos

--- java-backend-studies/01-fundamentos/src/ExerciciosCondicionais/ex06.java
+++ java-backend-studies/01-fundamentos/src/ExerciciosCondicionais/ex06.java
@@ -1,4 +1,7 @@
 package ExerciciosCondicionais;
 //Faça um programa que leia três números e mostre o maior e o menor deles
 public class ex06 {
+    static void main() {
+        //iniciando o git
+    }
 }
