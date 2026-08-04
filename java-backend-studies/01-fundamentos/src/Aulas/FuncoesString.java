098f2249b4d471f038823e90c9feeee4e3d3b2e0
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-04 20:35:22 -0300

    aprendendo a remover espacos em branco ao final da string com trim

--- java-backend-studies/01-fundamentos/src/Aulas/FuncoesString.java
+++ java-backend-studies/01-fundamentos/src/Aulas/FuncoesString.java
@@ -4,15 +4,22 @@ import java.util.Locale;
 
 public class FuncoesString {
     static void main() {
-        String original = "Lucas Cavalcante 30 anos estudante de Analise e desenvolvimento de sistemas aprendendo java";
+        String original = "Lucas Cavalcante 30 anos estudante de Analise e desenvolvimento de sistemas aprendendo java       ";
 
         String s01 = original.toLowerCase();
         System.out.println("- String original: " + original + " -");
         System.out.println("- toLowercase: " + s01 + " -");
+        System.out.println();
 
         String s02 = original.toUpperCase();
         System.out.println("- String original: " + original + " -");
         System.out.println("- toUppercase: " + s02 + " -");
+        System.out.println();
+
+        String s03 = original.trim();
+        System.out.println("- String original: " + original + " -");
+        System.out.println("- removendo espaços em branco no final: " + s03 + " -");
+        System.out.println();
 
     }
 }
