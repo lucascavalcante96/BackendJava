477942aa804dfaf51f4bd1aad78529131c726f42
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-04 20:26:32 -0300

    aprendendo o split

---
+++ java-backend-studies/01-fundamentos/src/ExercicioEstruturaRepetitiva/ex001.java
@@ -0,0 +1,16 @@
+package ExercicioEstruturaRepetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+// Faça um programa que peça uma nota, entre zero e dez.
+// Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+public class ex001 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String nome = "Lucas Cavalcante";
+        System.out.println(nome.split(" "));
+    }
+}
