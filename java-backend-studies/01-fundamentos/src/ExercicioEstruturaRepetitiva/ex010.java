3bec34c694214261d80e337ad61c3dec2c7d32b5
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-07 09:53:53 -0300

    exercicio tabuada

--- java-backend-studies/01-fundamentos/src/ExercicioEstruturaRepetitiva/ex010.java
+++ java-backend-studies/01-fundamentos/src/ExercicioEstruturaRepetitiva/ex010.java
@@ -1,4 +1,7 @@
 package ExercicioEstruturaRepetitiva;
+
+import java.util.Scanner;
+
 /*
 Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
  O usuário deve informar de qual numero ele deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
@@ -10,4 +13,29 @@ Tabuada de 5:
 5 X 10 = 50
  */
 public class ex010 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("Digite um numero entre 1 e 10: ");
+        numero = sc.nextInt();
+        int i = 1;
+        while(true){
+
+            if (numero > 0 && numero <= 10) {
+                if (i == 1){
+                    System.out.println("Tabuada de " + numero);
+                }
+                int resultado = numero * i;
+                System.out.printf("%d X %d = %d%n", numero,i, resultado);
+                i +=1;
+                if (i == 11){
+                    break;
+                }
+            } else {
+                System.out.println("Numero invalido");
+                System.out.println("Digite um numero entre 1 e 10: ");
+                numero = sc.nextInt();
+            }
+        }
+    }
 }
