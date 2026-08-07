7a5d49ac2416ef412a8dab5580fecc1123ee3b46
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-07 07:41:08 -0300

    ajustando a logica para que o programa fique mais facil de se ler e executar

--- java-backend-studies/01-fundamentos/src/ExercicioEstruturaRepetitiva/ex003.java
+++ java-backend-studies/01-fundamentos/src/ExercicioEstruturaRepetitiva/ex003.java
@@ -20,43 +20,57 @@ public class ex003 {
         char estadoCivil;
         int idade;
         double salario;
-        while(true){
-            System.out.println("Digite seu nome: ");
+
+        System.out.println("Digite seu nome: ");
+        nome = sc.nextLine();
+        while (nome.length() <= 3) {
+            System.out.println("Nome invalido digite novamente");
             nome = sc.nextLine();
-            if (nome.length() >= 3){
-                System.out.println("Digite sua idade: ");
-                idade = sc.nextInt();
-                if (idade >= 0 && idade <=150){
-                    System.out.println("Digite seu Estado Civil ");
-                    System.out.println("[S] para Solteiro/a");
-                    System.out.println("[C] para Casado/a");
-                    System.out.println("[V] para Viuvo/a");
-                    System.out.println("[D] para Divorciado/a");
-                    estadoCivil = sc.next().charAt(0);
-                    estadoCivil = Character.toLowerCase(estadoCivil);
+        }
 
-                    if (estadoCivil == 's' || estadoCivil == 'c' || estadoCivil == 'v' || estadoCivil == 'd'){
-                        System.out.println("Digite seu salario: ");
-                        salario = sc.nextDouble();
+        System.out.println("Digite sua idade: ");
+        idade = sc.nextInt();
+        while (idade < 0 || idade > 150) {
+            System.out.println("Idade invalida digite novamente");
+            idade = sc.nextInt();
+        }
 
-                        if (salario >= 0){
-                            System.out.println("Olá " + nome);
-                            System.out.println("Sua idade é de " + idade + " anos");
-                            System.out.println("Seu estado Civil é: " + estadoCivil);
-                            System.out.println("Seu salário é de R$ " + salario);
-                            break;
-                        }
-                    }
-                    else {
-                        System.out.println("Estado Civil invalido digite novamente !");
-                    }
-                }
-                else {
-                    System.out.println("Idade invalida digite novamente !");
-                }
+        System.out.println("Digite seu salario: ");
+        salario = sc.nextDouble();
+        while (salario <= 0) {
+            System.out.println("Salario invalido digite novamente");
+            salario = sc.nextDouble();
+        }
+
+        System.out.println("Digite seu estado civil: ");
+        System.out.println("[S] para Solteiro/a");
+        System.out.println("[C] para Casado/a");
+        System.out.println("[V] para Viuvo/a");
+        System.out.println("[D] para Divorciado/a");
+        estadoCivil = sc.next().charAt(0);
+        estadoCivil = Character.toLowerCase(estadoCivil);
+
+        while (true) {
+            if (estadoCivil == 's' || estadoCivil == 'c' || estadoCivil == 'v' || estadoCivil == 'd'){
+                break;
             } else {
-                System.out.println("Nome invalido digite novamente !");
+                System.out.println("Estado civil invalido digite novamente");
+                estadoCivil = sc.next().charAt(0);
+                estadoCivil = Character.toLowerCase(estadoCivil);
             }
+        }
+        System.out.printf("Seu nome %s%n", nome);
+        System.out.printf("Você tem %d anos %n", idade);
+        System.out.printf("Seu salário é de %.2f%n", salario);
+        if (estadoCivil == 's') {
+            System.out.println("Seu estado cívil é Solteiro");
+        } else if (estadoCivil == 'c') {
+            System.out.println("Seu estado cívil é Casado");
+        } else if (estadoCivil == 'v') {
+            System.out.println("Seu estado cívil é Viuvo/a");
+        } else {
+            System.out.println("Seu estado cívil é Divorciado");
         }
     }
 }
+
