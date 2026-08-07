dca2fa15c2b12242b5c3fd2f2befbf1cc676faf9
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-07 09:15:29 -0300

    exercicio onde usuario digita 2 numeros e imprimo os numeros entre eles

---
+++ java-backend-studies/01-fundamentos/src/ExercicioEstruturaRepetitiva/ex008.java
@@ -0,0 +1,33 @@
+package ExercicioEstruturaRepetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+public class ex008 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numero1, numero2;
+        int maior, menor;
+
+        System.out.println("Digite o primeiro numero: ");
+        numero1 = sc.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        numero2 = sc.nextInt();
+
+        if (numero1 < numero2){
+            menor = numero1;
+            maior = numero2;
+        } else {
+            menor = numero2;
+            maior = numero1;
+        }
+
+        for (int i = menor +1; i <maior ; i++) {
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
