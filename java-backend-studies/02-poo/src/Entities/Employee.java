facd5bd6050144fc619fa93d525f17294db4f0a3
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-08 09:43:41 -0300

    exercicio de fixacao sobre POO

---
+++ java-backend-studies/02-poo/src/Entities/Employee.java
@@ -0,0 +1,15 @@
+package Entities;
+
+public class Employee {
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary(){
+        return grossSalary - tax;
+    }
+
+    public void increaseSalary(double amount){
+        grossSalary += grossSalary * (amount / 100);
+    }
+}
