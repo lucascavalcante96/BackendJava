253a4554c3476f52fec45599cdb38f945857d1e8
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-07 22:27:06 -0300

    POO reaproveitamento de codigo e delegacao de responsabilidades

--- java-backend-studies/02-poo/src/aulas/POO.java
+++ java-backend-studies/02-poo/src/aulas/POO.java
@@ -11,7 +11,6 @@ public class POO {
         Triangulo x, y;
         x = new Triangulo();
         y = new Triangulo();
-
         System.out.println("Digite o valor do triangulo: ");
         x.a = sc.nextDouble();
         x.b = sc.nextDouble();
@@ -21,14 +20,15 @@ public class POO {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2;
-        double areaX = Math.sqrt(p * (p - x.a) *(p - x.b) * (p - x.c));
-
-        p = (y.a + y.b + y.c) / 2;
-        double areaY = Math.sqrt(p * (p - y.a) *(p - y.b) * (p - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Area do Triangulo X: %.3f%n", areaX);
         System.out.printf("Area do Triangulo Y: %.3f%n", areaY);
-
+        if (areaX > areaY) {
+            System.out.println("Maior area X");
+        }  else {
+            System.out.println("Maior area: Y");
+        }
     }
 }
