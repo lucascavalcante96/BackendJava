31eea776b690fb8822afe785284ffae9e8d0c9f5
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-11 21:03:37 -0300

    melhorando o exercicio com construtores e encapsulamento

--- java-backend-studies/02-poo/src/Entities/Rectangle.java
+++ java-backend-studies/02-poo/src/Entities/Rectangle.java
@@ -1,10 +1,29 @@
 package Entities;
 
 public class Rectangle {
-        public double width;
-        public double height;
+        private double width;
+        private double height;
 
-        public double area() {
+    public Rectangle() {
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double area() {
             return width * height;
         }
         public double perimeter(){
--- java-backend-studies/02-poo/src/exerciciosPoo/Programa.java
+++ java-backend-studies/02-poo/src/exerciciosPoo/Programa.java
@@ -9,14 +9,16 @@ public class Programa {
     static void main() {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Rectangle retangulo = new Rectangle();
 
         System.out.println("Entre com a altura do retangulo: ");
-        retangulo.height = sc.nextDouble();
+        double height = sc.nextDouble();
 
         System.out.println("Entre com a largura do retangulo: ");
-        retangulo.width = sc.nextDouble();
+        double width = sc.nextDouble();
 
+        Rectangle retangulo = new Rectangle();
+        retangulo.setHeight(height);
+        retangulo.setWidth(width);
         System.out.printf("AREA = %.2f%n", retangulo.area());
         System.out.printf("PERIMETRO = %.2f%n", retangulo.perimeter());
         System.out.printf("DIAGONAL = %.2f%n", retangulo.diagonal());
