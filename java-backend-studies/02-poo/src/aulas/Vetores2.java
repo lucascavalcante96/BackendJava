e21dcd5df73fdd2e83ff10ad675c725b04351645
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-13 20:38:44 -0300

    exercicio sobre vetores com OO

---
+++ java-backend-studies/02-poo/src/aulas/Vetores2.java
@@ -0,0 +1,36 @@
+package aulas;
+
+import Entities.Product2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vetores2 {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite quantos produtos deseja inserir: ");
+        int n = sc.nextInt();
+
+        Product2[] vect = new Product2[n];
+        for (int i = 0; i < n; i++) {
+            sc.nextLine();
+            System.out.println("Nome do produto: ");
+            String nome = sc.nextLine();
+            System.out.println("Preco do produto: ");
+            double preco = sc.nextDouble();
+
+            vect[i] = new Product2(nome, preco);
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < n; i++) {
+            soma += vect[i].getPreco();
+        }
+
+        double media = soma / n;
+        System.out.printf("MEDIA = %.2f", media);
+        sc.close();
+    }
+}
