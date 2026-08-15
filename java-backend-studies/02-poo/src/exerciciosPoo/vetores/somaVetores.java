3862d70801308952ffe3b6a6b7c77dd8e44dcf11
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-14 22:12:30 -0300

    adicionando o enunciado do exercicio

--- java-backend-studies/02-poo/src/exerciciosPoo/vetores/somaVetores.java
+++ java-backend-studies/02-poo/src/exerciciosPoo/vetores/somaVetores.java
@@ -1,5 +1,10 @@
 package exerciciosPoo.vetores;
 
+/*
+Faça um programa para ler dois vetores A e B, contendo N elementos cada. Em seguida, gere um
+terceiro vetor C onde cada elemento de C é a soma dos elementos correspondentes de A e B. Imprima
+o vetor C gerado.
+ */
 import java.util.Locale;
 import java.util.Scanner;
 
