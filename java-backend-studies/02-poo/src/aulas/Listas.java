bf5144f1544b9fedd71e0d995c79af7db57484f6
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-16 19:21:28 -0300

    aprendendo a remover um item, ver o indice e filtrar atraves de uma lista

--- java-backend-studies/02-poo/src/aulas/Listas.java
+++ java-backend-studies/02-poo/src/aulas/Listas.java
@@ -2,6 +2,7 @@ package aulas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Listas {
     static void main() {
@@ -18,6 +19,30 @@ public class Listas {
         for (String str : list) {
             System.out.println(str);
         }
+        System.out.println("-----------------------");
+        list.remove("Flávio");
+        list.removeIf(x -> x.charAt(0) == 'L');
+
+        for (String str : list) {
+            System.out.println(str);
+        }
+
+        System.out.println("-----------------------");
+
+        System.out.println("Index of Maria " + list.indexOf("Marcia")); //como não existe irá retornar -1
+        System.out.println("Index of Alex " + list.indexOf("Alex"));
+
+        System.out.println("-----------------------");
+
+        //filtrando uma lista para que me retorne uma nova lista com os itens que comecam com X parametro
+        List<String> resultado =  list.stream().filter(x -> x.charAt(0) == 'G').collect(Collectors.toList());
+        //primeiro converto a lista para stream, depois filtro fazendo uma funcao lambda e depois retorno para uma lista novamente
+        //usando a funcao colect
+
+        for (String str : resultado) {
+            System.out.println(str);
+        }
+
     }
 
 }
