789f74c716a22bed3c50e3c76beefed3406f3ef1
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-16 12:15:14 -0300

    aprendendo a fazer boxing

---
+++ java-backend-studies/02-poo/src/aulas/Boxing.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class Boxing {
+    static void main() {
+        int x = 20;
+        // fazendo boxing
+        Object obj = x;
+        System.out.println(obj);
+
+    }
+}
