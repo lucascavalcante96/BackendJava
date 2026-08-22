29d7ac786620c903d04d3a1d6b613a98c2cba092
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-22 18:06:25 -0300

    exercicio sobre listas, fazendo ele de forma robusta e deixando o escopo mais limpo

--- java-backend-studies/02-poo/src/exerciciosPoo/Listas/Entities/Suspeito.java
+++ java-backend-studies/02-poo/src/exerciciosPoo/Listas/Entities/Suspeito.java
@@ -7,6 +7,9 @@ public class Suspeito {
     private char moraPerto;
     private char devia;
     private char jaTrabalhou;
+    private int participacao;
+    private String culpado;
+
 
     public Suspeito(String nome, char telefonou, char esteveNoLocal, char moraPerto, char devia, char jaTrabalhou) {
         this.nome = nome;
@@ -15,21 +18,43 @@ public class Suspeito {
         this.moraPerto = moraPerto;
         this.devia = devia;
         this.jaTrabalhou = jaTrabalhou;
+        participacao = participouDoCrime(telefonou, esteveNoLocal,moraPerto, devia, jaTrabalhou);
+        culpado = indiceDeParticipacao(participacao);
     }
 
-    int indiceDeParticipacao = 0;
-    public void participouDoCrime(char telefonou, char esteveNoLocal, char moraPerto, char devia, char jaTrabalhou) {
-        if (telefonou == 'S'){
+
+    private int participouDoCrime(char telefonou, char esteveNoLocal, char moraPerto, char devia, char jaTrabalhou) {
+        int indiceDeParticipacao = 0;
+        if (Character.toLowerCase(telefonou) == 's'){
             indiceDeParticipacao++;
-        }if (esteveNoLocal == 'S'){
+        }if (Character.toLowerCase(esteveNoLocal) == 's'){
             indiceDeParticipacao++;
-        }if (moraPerto == 'S'){
+        }if (Character.toLowerCase(moraPerto) == 's'){
             indiceDeParticipacao++;
-        }if (devia == 'S'){
+        }if (Character.toLowerCase(devia) == 's'){
             indiceDeParticipacao++;
-        }if (jaTrabalhou == 'S'){
+        }if (Character.toLowerCase(jaTrabalhou) == 's'){
             indiceDeParticipacao++;
         }
+        return indiceDeParticipacao;
+    }
+
+    private String indiceDeParticipacao(int indiceDeParticipacao) {
+        if (indiceDeParticipacao <= 1){
+            return "Inocente";
+        } else if (indiceDeParticipacao == 2){
+            return "Suspeito";
+        } else if (indiceDeParticipacao > 2 && indiceDeParticipacao <=4) {
+            return "Cúmplice";
+        }
+        else {
+            return "Assassino";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Suspeito{" + nome + " esta classificado como: " +  culpado + '}';
     }
 
 }
--- java-backend-studies/02-poo/src/exerciciosPoo/Listas/ex003.java
+++ java-backend-studies/02-poo/src/exerciciosPoo/Listas/ex003.java
@@ -17,9 +17,15 @@ Utilizando listas faça um programa que faça 5 perguntas para uma pessoa sobre 
 O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
 Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e
 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
+
+OBS: Acrescentei um desafio a mais no exercicio, onde ele entrevista X pessoas e coloca na lista, foi criado uma classe
+Suspeito para armazenar os dados e decidir o grau no assassinato
  */
 public class ex003 {
     static void main() {
+        String nome;
+        char telefonou,esteveNoLocal, moraPerto, devia, jaTrabalhou;
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite quantos suspeitos serão entrevistados: ");
@@ -28,23 +34,36 @@ public class ex003 {
         List<Suspeito> suspeitos = new ArrayList<>();
 
         for (int i = 0; i < qtdSuspeitos; i++) {
-            System.out.println("Digite o nome do suspeito " + (i + 1) + ":");
-            String nome = sc.nextLine();
-            System.out.println("Responda o questionário abaixo com S para SIM ou N para Não");
-            System.out.print("Telefonou para a vitima? [S/N]");
-            char telefonou = sc.next().charAt(0);
-            System.out.print("Esteve no local do crime? [S/N]");
-            char esteveNoLocal =sc.next().charAt(0);
-            System.out.print("Mora perto da vítima?  [S/N]");
-            char moraPerto =sc.next().charAt(0);
-            System.out.print("Devia para a vítima? [S/N]");
-            char devia =sc.next().charAt(0);
-            System.out.print("Já trabalhou com a vítima?  [S/N]");
-            char jaTrabalhou =sc.next().charAt(0);
+            sc.nextLine();
 
+            System.out.print("Digite o nome do suspeito " + (i + 1) + ":");
+            nome = sc.nextLine();
+
+            System.out.println("Responda o questionário abaixo com S para SIM ou N para Não");
+
+            telefonou = lerResposta(sc, "Telefonou para a vitima? [S/N]");
+            esteveNoLocal = lerResposta(sc,"Esteve no local do crime? [S/N]");
+            moraPerto = lerResposta(sc,"Mora perto da vítima?  [S/N]");
+            devia = lerResposta(sc,"Devia para a vítima? [S/N]");
+            jaTrabalhou = lerResposta(sc,"Já trabalhou com a vítima?  [S/N]");
+
+            Suspeito suspeito = new Suspeito(nome, telefonou, esteveNoLocal, moraPerto, devia, jaTrabalhou);
+
+            suspeitos.add(suspeito);
         }
 
+        for (Suspeito suspeito : suspeitos) {
+            System.out.println(suspeito);
+        }
         sc.close();
-
+    }
+    private static char lerResposta(Scanner sc, String pergunta) {
+        System.out.print(pergunta);
+        char resposta = sc.next().charAt(0);
+        while (Character.toLowerCase(resposta) != 's' && Character.toLowerCase(resposta) != 'n'){
+            System.out.println("Digite S ou N");
+            resposta = sc.next().charAt(0);
+        }
+        return resposta;
     }
 }
