4befd3c527a30564a083a0d0c421f9c3c2f8afbe
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-23 18:43:07 -0300

    localdateTime

--- java-backend-studies/01-fundamentos/src/Aulas/DataHora.java
+++ java-backend-studies/01-fundamentos/src/Aulas/DataHora.java
@@ -1,10 +1,14 @@
 package Aulas;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DataHora {
     static void main() {
         LocalDate data = LocalDate.now();
         System.out.println("Data: " + data);
+
+        LocalDateTime d02 = LocalDateTime.now();
+        System.out.println("Data e Hora: " + d02);
     }
 }
