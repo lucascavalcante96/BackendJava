a2821dd0d82b7f0c48118643e2f5f7655652f2a2
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-23 18:26:05 -0300

    aprendendo fundamentos de data e hora em java com localdate

---
+++ java-backend-studies/01-fundamentos/src/Aulas/DataHora.java
@@ -0,0 +1,10 @@
+package Aulas;
+
+import java.time.LocalDate;
+
+public class DataHora {
+    static void main() {
+        LocalDate data = LocalDate.now();
+        System.out.println("Data: " + data);
+    }
+}
