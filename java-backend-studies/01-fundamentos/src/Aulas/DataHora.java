9c5e5aa37352e9da1c0ac830786a016390367238
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-23 18:54:10 -0300

    mais alguns pontos sobre DataHora

--- java-backend-studies/01-fundamentos/src/Aulas/DataHora.java
+++ java-backend-studies/01-fundamentos/src/Aulas/DataHora.java
@@ -13,5 +13,20 @@ public class DataHora {
         System.out.println("Data e Hora: " + d02);
         Instant instant = Instant.now();
         System.out.println("Data de instante: " + instant);
+
+        LocalDate d03 = LocalDate.parse("1996-01-31"); // criando uma data no padrao iso 8601
+        System.out.println("Data de nascimento " + d03);
+
+        LocalDateTime d04 = LocalDateTime.parse("2026-01-31T00:00:00"); // Criando uma dataHora no formato ISO 8601
+        System.out.println(d04);
+
+
+        Instant d05 = Instant.parse("2026-01-31T00:00:00Z"); // instanciando pelo horario gmt
+        System.out.println(d05);
+
+        Instant d06 = Instant.parse("2026-01-31T00:00:00-03:00"); //instanciando pelo horario de brasilia
+        System.out.println(d06);//imprime com 3 horas a mais
+
+
     }
 }
