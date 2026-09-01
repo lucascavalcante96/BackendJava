605012556845092293215515970cbb63f3edc644
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-08-31 22:19:32 -0300

    aprendendo downcasting

--- java-backend-studies/02-poo/src/aulas/heranca.java
+++ java-backend-studies/02-poo/src/aulas/heranca.java
@@ -9,17 +9,30 @@ import Entities.SavingsAccount;
 public class heranca {
     static void main() {
 
-    Account acc = new Account(1001, "Alex", 0.0);
+        Account acc = new Account(1001, "Alex", 0.0);
 
-    BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
-    // UPCASTING
+        // UPCASTING
 
         Account acc1 = bacc;
-
         Account acc2 = new BusinessAccount(1003, "Joao", 0.0, 200.0);
         Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
 
+        // DOWNCASTING
 
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+
+        if (acc3 instanceof BusinessAccount) {
+            BusinessAccount bacc1 = (BusinessAccount) acc3;
+            bacc1.loan(200.0);
+            System.out.println("Loan Successful");
+        }
+
+        if (acc3 instanceof SavingsAccount) {
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Balance Updated");
+        }
     }
 }
