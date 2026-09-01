8464b7edff9a2e38016c26c0594896b6075c2402
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-09-01 14:02:46 -0300

    colocando os getters e setters e o constructor padrao

--- java-backend-studies/02-poo/src/exerciciosPoo/Heranca/Entities/OutsourcedEmployee.java
+++ java-backend-studies/02-poo/src/exerciciosPoo/Heranca/Entities/OutsourcedEmployee.java
@@ -3,8 +3,20 @@ package exerciciosPoo.Heranca.Entities;
 public class OutsourcedEmployee extends Employee {
     private Double additionalCharge;
 
+    public OutsourcedEmployee() {
+        super();
+    }
+
     public OutsourcedEmployee(String nome, Integer hours, Double valuePerHour, Double additionalCharge) {
         super(nome, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
         this.additionalCharge = additionalCharge;
     }
 
