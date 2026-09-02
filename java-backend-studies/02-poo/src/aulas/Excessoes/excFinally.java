a5306492518846f504c86085fe91e23bbf2f7630
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-09-02 20:43:40 -0300

    aprendendo o finally

---
+++ java-backend-studies/02-poo/src/aulas/Excessoes/excFinally.java
@@ -0,0 +1,27 @@
+package aulas.Excessoes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class excFinally {
+    static void main() {
+        File file = new File("D:\\test.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+                System.out.println("File closed");
+            }
+        }
+    }
+}
