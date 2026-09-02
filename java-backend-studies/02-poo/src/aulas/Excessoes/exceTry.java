d669a31312a4271ac011c2aa255e0630097d0ea5
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-09-02 20:22:02 -0300

    iniciando em excessoes com try catch

---
+++ java-backend-studies/02-poo/src/aulas/Excessoes/exceTry.java
@@ -0,0 +1,26 @@
+package aulas.Excessoes;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//Praticando excessões do tipo Try-Catch
+public class exceTry {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String [] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+        System.out.println("End of program");
+        sc.close();
+    }
+}
