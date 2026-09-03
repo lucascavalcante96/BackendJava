ab1c13ca2abf60f15948f15a350ab6b300e4abd2
Lucas Cavalcante <lulucas.cavalcante@gmail.com>
2026-09-02 23:58:03 -0300

    excecoes personalizadas

---
+++ java-backend-studies/02-poo/src/aulas/Excecoes/Model/exceptions/DomainException.java
@@ -0,0 +1,10 @@
+package aulas.Excessoes.Model.exceptions;
+
+public class DomainException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+}
